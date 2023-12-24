da74701e8be379f2148acb6fd2b5a68389536df8
Ryunosuke1114 <ryunosuke1114@gmail.com>
2023-12-24 15:51:57 +0900

    feat: bottomNavigation

---
+++ lib/office/office_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OfficeView extends StatelessWidget {
+  const OfficeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+      child: Text('Office'),
+    ));
+  }
+}
