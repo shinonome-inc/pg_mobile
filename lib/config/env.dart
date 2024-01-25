a378be514a9d196fc432562dda7e3bd8d9eba1d2
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-01-25 23:34:16 +0900

    環境変数にPGN_API_BASE_URLを追加

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -6,4 +6,6 @@ part 'env.g.dart';
 abstract class Env {
   @EnviedField(varName: 'USE_DEBUG_MODE')
   static const bool useDebugMode = _Env.useDebugMode;
+  @EnviedField(varName: 'PGN_API_BASE_URL', obfuscate: true)
+  static const String pgnAPIBaseUrl = _Env.pgnAPIBaseUrl;
 }
