69af93fc6cef436e106bcbc1c99d1704b902a6fe
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-01-25 23:43:45 +0900

    Envクラスのコメントアウトを追加

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -2,6 +2,12 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
+/// .envファイルから読み取った環境変数を定数として扱うためのクラスです。
+///
+/// enviedを用いて.envから読み取った環境変数を難読化/暗号化してアプリに組み込むため、
+/// 環境変数を難読化/暗号化せずアプリに組み込むdote_envより安全に管理できます。
+///
+/// 難読化/暗号化を行うには、@EnviedFieldのobfuscateをtrueに設定してください。
 @Envied(path: '.env')
 abstract class Env {
   @EnviedField(varName: 'USE_DEBUG_MODE')
