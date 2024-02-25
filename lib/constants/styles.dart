9a6fcefd5d174fa46e9334a891dfbcefb5d1e9d7
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-02-25 11:41:42 +0900

    Stylesクラスを作成してColorを定義

---
+++ lib/constants/styles.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+class Styles {
+  static const Color gray1 = Color(0xFF222222);
+  static const Color gray2 = Color(0xFF353535);
+  static const Color gray3 = Color(0xFF828282);
+  static const Color gray4 = Color(0xFFDBDBDB);
+  static const Color gray5 = Color(0xFFE6E4E7);
+  static const Color gray6 = Color(0xFFF2F2F2);
+
+  static const Color gray1A80 = Color(0xCC222222);
+  static const Color gray5A80 = Color(0xCCE6E4E7);
+
+  static const Color accent = Color(0xFF05A59D);
+  static const Color deepAccent = Color(0xFF057D75);
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color blue = Color(0xFF0081B9);
+  static const Color yellow = Color(0xFFF2C94C);
+  static const Color red = Color(0xFFEB5757);
+}
