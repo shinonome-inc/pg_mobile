19eea3d6947ee130d970119739a32d400429e927
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-02-25 22:54:27 +0900

    lib/firebase_options.dartを追加

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -14,4 +14,14 @@ abstract class Env {
   static const bool useDebugMode = _Env.useDebugMode;
   @EnviedField(varName: 'PGN_API_BASE_URL', obfuscate: true)
   static String pgnAPIBaseUrl = _Env.pgnAPIBaseUrl;
+  @EnviedField(varName: 'FIREBASE_API_KEY_ANDROID', obfuscate: true)
+  static String firebaseAPIKeyAndroid = _Env.firebaseAPIKeyAndroid;
+  @EnviedField(varName: 'FIREBASE_APP_ID_ANDROID', obfuscate: true)
+  static String firebaseAppIdAndroid = _Env.firebaseAppIdAndroid;
+  @EnviedField(varName: 'FIREBASE_API_KEY_IOS', obfuscate: true)
+  static String firebaseAPIKeyIOS = _Env.firebaseAPIKeyIOS;
+  @EnviedField(varName: 'FIREBASE_APP_ID_IOS', obfuscate: true)
+  static String firebaseAppIdIOS = _Env.firebaseAppIdIOS;
+  @EnviedField(varName: 'FIREBASE_MESSAGING_SENDER_ID', obfuscate: true)
+  static String firebaseMessagingSenderId = _Env.firebaseMessagingSenderId;
 }
