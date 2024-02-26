bc2e56d521e275af17415ff6f808ae742e1f51a5
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-02-26 19:39:57 +0900

    MastdonRepositoryクラスを作成定義してsignInメソッドを定義

--- lib/debug/login_sample/login_button.dart
+++ lib/debug/login_sample/login_button.dart
@@ -1,46 +1,15 @@
-import 'package:flutter_web_auth/flutter_web_auth.dart';
-import 'package:pg_mobile/debug/util/env_mixin.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:pg_mobile/debug/util/env_mixin.dart';
+import 'package:pg_mobile/repository/mastdon_repository.dart';
 
-class LoginButton extends StatelessWidget with EnvMixin{
+class LoginButton extends StatelessWidget with EnvMixin {
   const LoginButton({super.key});
-
-  Future<void> signInWithMastodon() async {
-    final url = Uri.parse('$mastodonInstanceUrl/oauth/authorize?client_id=$mastodonClientId&response_type=code&redirect_uri=$mastodonRedirectUri&scope=read');
-
-    final result = await FlutterWebAuth.authenticate(
-        url: url.toString(),
-        callbackUrlScheme: "pgmobile");
-
-    final code = Uri.parse(result).queryParameters['code'];
-    if (code != null) {
-      final response = await http.post(
-        Uri.parse('$mastodonInstanceUrl/oauth/token'),
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-        body: {
-          'client_id': mastodonClientId,
-          'client_secret': mastodonClientSecret,
-          'grant_type': 'authorization_code',
-          'code': code,
-          'redirect_uri': mastodonRedirectUri,
-        },
-      );
-
-      final accessToken = json.decode(response.body)['access_token'];
-      if (accessToken != null) {
-        // アクセストークンを使用して、Mastodon APIの認証が必要な操作を行う
-        debugPrint('Access Token: $accessToken');
-      }
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {
-        signInWithMastodon();
+      onPressed: () async {
+        await MastdonRepository.signIn();
       },
       child: const Text('Sign in with Mastodon'),
     );
--- lib/repository/mastdon_repository.dart
+++ lib/repository/mastdon_repository.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_auth/flutter_web_auth.dart';
 import 'package:http/http.dart' as http;
+import 'package:pg_mobile/config/env.dart';
 import 'package:pg_mobile/debug/util/env_mixin.dart';
 
 class MastdonRepository with EnvMixin {
@@ -10,7 +11,7 @@ class MastdonRepository with EnvMixin {
 
   static Future<void> signIn() async {
     final url = Uri.parse(
-        '$mastodonInstanceUrl/oauth/authorize?client_id=$mastodonClientId&response_type=code&redirect_uri=$mastodonRedirectUri&scope=read');
+        '${Env.mastodonInstanceUrl}/oauth/authorize?client_id=${Env.mastodonClientId}&response_type=code&redirect_uri=${Env.mastodonRedirectUri}&scope=read');
 
     final result = await FlutterWebAuth.authenticate(
         url: url.toString(), callbackUrlScheme: "pgmobile");
@@ -18,14 +19,14 @@ class MastdonRepository with EnvMixin {
     final code = Uri.parse(result).queryParameters['code'];
     if (code != null) {
       final response = await http.post(
-        Uri.parse('$mastodonInstanceUrl/oauth/token'),
+        Uri.parse('${Env.mastodonInstanceUrl}/oauth/token'),
         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
         body: {
-          'client_id': mastodonClientId,
-          'client_secret': mastodonClientSecret,
+          'client_id': Env.mastodonClientId,
+          'client_secret': Env.mastodonClientSecret,
           'grant_type': 'authorization_code',
           'code': code,
-          'redirect_uri': mastodonRedirectUri,
+          'redirect_uri': Env.mastodonRedirectUri,
         },
       );
 
