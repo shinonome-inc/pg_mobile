8f6a358174155c919e49d8d04cd6e7ca755f5354
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-02-26 09:31:01 +0900

    オフィスを横スクロールで切り替え

--- lib/debug/debug_office_page.dart
+++ lib/debug/debug_office_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pg_mobile/models/office.dart';
 import 'package:pg_mobile/models/office_user.dart';
 import 'package:pg_mobile/repository/firestore_repository.dart';
@@ -16,6 +15,8 @@ class _DebugOfficePageState extends State<DebugOfficePage> {
   late Stream<List<OfficeUser>> _officeUserStream;
   List<Office> _offices = [];
   List<OfficeUser> _officeUsers = [];
+
+  late PageController _pageViewController;
 
   Future<void> _officeListener(List<Office> newOffices) async {
     setState(() {
@@ -36,39 +37,54 @@ class _DebugOfficePageState extends State<DebugOfficePage> {
     _officeUserStream = FirestoreRepository.getOfficeUsersStream();
     _officeStream.listen(_officeListener);
     _officeUserStream.listen(_officeUserListener);
+
+    _pageViewController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageViewController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Debug Office Page'),
-      ),
-      body: ListView.builder(
-        itemCount: _offices.length,
-        itemBuilder: (context, officeIndex) {
-          final office = _offices[officeIndex];
-          return Column(
-            children: [
-              Text(office.toString()),
-              ListView.builder(
-                shrinkWrap: true,
-                itemCount: office.userIdList.length,
-                itemBuilder: (context, userIndex) {
-                  final user = _officeUsers.firstWhere(
-                    (user) => user.id == _officeUsers[userIndex].id,
-                  );
-                  return Column(
-                    children: [
-                      Text(user.toString()),
-                    ],
-                  );
-                },
-              ),
-              SizedBox(height: 16.h),
-            ],
-          );
-        },
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Debug Office Page'),
+        ),
+        body: PageView.builder(
+          controller: _pageViewController,
+          itemBuilder: (context, officeIndex) {
+            if (_offices.isEmpty) {
+              return const Center(
+                child: Text('オフィスがありません'),
+              );
+            }
+            final office = _offices[officeIndex % _offices.length];
+            return Column(
+              children: [
+                Text(office.toString()),
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: office.userIdList.length,
+                  itemBuilder: (context, userIndex) {
+                    final user = _officeUsers.firstWhere(
+                      (user) => user.id == _officeUsers[userIndex].id,
+                    );
+                    return Column(
+                      children: [
+                        Text(user.toString()),
+                      ],
+                    );
+                  },
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
