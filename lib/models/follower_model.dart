8036559a35938af0dea7d9f87941456fce9582f8
Yoshida-koshi <koshi2278@outlook.jp>
2024-02-27 02:22:42 +0900

    jsonkeyを削除

--- lib/models/follower_model.dart
+++ lib/models/follower_model.dart
@@ -6,12 +6,9 @@ part 'follower_model.g.dart';
 @freezed
 class FollowerModel with _$FollowerModel {
   const factory FollowerModel({
-    // ignore: invalid_annotation_target
-    @JsonKey(name: 'display_name') required String displayName,
-    //ignore: invalid_annotation_target
-    @JsonKey(name: 'username') required String username,
-    // ignore: invalid_annotation_target
-    @JsonKey(name: 'avatar') required String avatarUrl,
+    required String display_name,
+    required String username,
+    required String avatar,
   }) = _FollowerModel;
 
   factory FollowerModel.fromJson(Map<String, dynamic> json) =>
