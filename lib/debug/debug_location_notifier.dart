e272f56afdf075383a492c553045887e48c5e785
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-02-27 21:51:11 +0900

    DebutLocationNotifierクラスを追加

---
+++ lib/debug/debug_location_notifier.dart
@@ -0,0 +1,89 @@
+import 'dart:async';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:location/location.dart' as location;
+import 'package:permission_handler/permission_handler.dart';
+import 'package:pg_mobile/constants/locations.dart';
+import 'package:pg_mobile/debug/debug_location_state.dart';
+import 'package:pg_mobile/util/location_util.dart';
+
+final debugLocationProvider =
+    StateNotifierProvider<DebugLocationNotifier, DebugLocationState>(
+  (ref) => DebugLocationNotifier(ref),
+);
+
+class DebugLocationNotifier extends StateNotifier<DebugLocationState> {
+  DebugLocationNotifier(this.ref) : super(defaultDebugLocationState) {
+    _checkLocationPermission();
+    _setLocationListener();
+  }
+
+  final Ref ref;
+
+  final _location = location.Location();
+
+  void _setLoading(bool value) {
+    state = state.copyWith(isLoading: value);
+  }
+
+  void _setLocationPermissionStatus(PermissionStatus status) {
+    state = state.copyWith(status: status);
+  }
+
+  void _switchCheckingIn() {
+    state = state.copyWith(isCheckingIn: !state.isCheckingIn);
+  }
+
+  void _setCurrentLatLong(double currentLat, double currentLong) {
+    state = state.copyWith(
+      currentLat: currentLat,
+      currentLong: currentLong,
+    );
+  }
+
+  Future<void> _checkLocationPermission() async {
+    if (state.isLoading) return;
+    _setLoading(true);
+    final status = await Permission.locationAlways.request();
+    _setLocationPermissionStatus(status);
+    _setLoading(false);
+  }
+
+  Future<void> checkInCheckOut() async {
+    if (state.isLoading) return;
+    _setLoading(true);
+    await Future.delayed(const Duration(seconds: 1));
+    _switchCheckingIn();
+    _setLoading(false);
+  }
+
+  Future<void> openSettings() async {
+    _setLoading(true);
+    final status = await Permission.locationAlways.status;
+    _setLocationPermissionStatus(status);
+    if (!status.isGranted) {
+      await openAppSettings();
+    } else {
+      _setLocationListener();
+    }
+    _checkLocationPermission();
+    _setLoading(false);
+  }
+
+  void _setLocationListener() {
+    _location.onLocationChanged.listen(_locationListener);
+  }
+
+  void _locationListener(location.LocationData currentLocation) {
+    if (currentLocation.latitude == null || currentLocation.longitude == null) {
+      return;
+    }
+    _setCurrentLatLong(currentLocation.latitude!, currentLocation.longitude!);
+    state = state.copyWith(
+      distanceInMeters: LocationUtil.distanceInMeters(
+        currentLocation,
+        Locations.debugTarget,
+      ),
+    );
+  }
+}
