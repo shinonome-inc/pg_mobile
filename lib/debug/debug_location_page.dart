f46e56ee58724b0ef2ed437092efd1ed51db8d8b
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-02-27 16:28:33 +0900

    オフィスと現在地の距離を画面に表示

--- lib/debug/debug_location_page.dart
+++ lib/debug/debug_location_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:location/location.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:pg_mobile/debug/debug_loding_view.dart';
+import 'package:pg_mobile/util/location_util.dart';
 import 'package:pg_mobile/util/navigator_util.dart';
 
 class DebugLocationPage extends StatefulWidget {
@@ -16,7 +18,16 @@ class _DebugLocationPageState extends State<DebugLocationPage> {
   bool _isCheckingIn = false;
   double _currentLat = 0.0;
   double _currentLong = 0.0;
+  double _distanceInMeters = 0.0;
   final _location = Location();
+
+  // PlayGround東京オフィスの座標
+  final _targetLocation = LocationUtil.convertToLocationData(
+    35.7021178,
+    139.7449061,
+  );
+
+  bool get _isInOffice => _distanceInMeters <= 10.0;
 
   void _setLoading(bool value) {
     setState(() {
@@ -35,14 +46,6 @@ class _DebugLocationPageState extends State<DebugLocationPage> {
       _currentLat = currentLat;
       _currentLong = currentLong;
     });
-  }
-
-  void _locationListener(LocationData currentLocation) {
-    if (currentLocation.latitude == null || currentLocation.longitude == null) {
-      return;
-    }
-    _setCurrentLatLong(currentLocation.latitude!, currentLocation.longitude!);
-    print('lat: $_currentLat, long: $_currentLong');
   }
 
   void _onPressedCancel() {
@@ -85,6 +88,19 @@ class _DebugLocationPageState extends State<DebugLocationPage> {
     }
   }
 
+  void _locationListener(LocationData currentLocation) {
+    if (currentLocation.latitude == null || currentLocation.longitude == null) {
+      return;
+    }
+    _setCurrentLatLong(currentLocation.latitude!, currentLocation.longitude!);
+    setState(() {
+      _distanceInMeters = LocationUtil.distanceInMeters(
+        currentLocation,
+        _targetLocation,
+      );
+    });
+  }
+
   @override
   void initState() {
     super.initState();
@@ -99,10 +115,28 @@ class _DebugLocationPageState extends State<DebugLocationPage> {
       ),
       body: Stack(
         children: [
-          Center(
-            child: ElevatedButton(
-              onPressed: _onPressedCheckInCheckOut,
-              child: Text(_isCheckingIn ? 'チェックアウト' : 'チェックイン'),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Spacer(),
+                Text(
+                    'オフィスの座標: ${_targetLocation.latitude!.toStringAsFixed(5)}, ${_targetLocation.longitude!.toStringAsFixed(5)}'),
+                Text(
+                    '　現在地の座標: ${_currentLat.toStringAsFixed(5)}, ${_currentLong.toStringAsFixed(5)}'),
+                const Divider(),
+                Text('オフィスまでの距離: ${_distanceInMeters.toStringAsFixed(2)}(m)'),
+                const Spacer(),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: _onPressedCheckInCheckOut,
+                    child: Text(_isCheckingIn ? 'チェックアウト' : 'チェックイン'),
+                  ),
+                ),
+                SizedBox(height: 32.h),
+              ],
             ),
           ),
           if (_isLoading) const DebugLoadingView(),
