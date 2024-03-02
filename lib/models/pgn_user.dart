762866972c1be9a5f188a75533409fb898198f9f
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-03-02 15:07:21 +0900

    PGNUserモデルのJsonKeyとデフォルト値を設定

--- lib/models/pgn_user.dart
+++ lib/models/pgn_user.dart
@@ -6,11 +6,12 @@ part 'pgn_user.g.dart';
 @freezed
 class PGNUser with _$PGNUser {
   const factory PGNUser({
-    required String userId,
-    required int total,
-    required int totalPgrit,
-    required int totalDawn,
-    required int totalOther,
+    @JsonKey(defaultValue: '') required String id,
+    @JsonKey(defaultValue: '') required String slackId,
+    @JsonKey(defaultValue: 0) required int total,
+    @JsonKey(name: 'total_pgrit', defaultValue: 0) required int totalPgrit,
+    @JsonKey(name: 'total_dawn', defaultValue: 0) required int totalDawn,
+    @JsonKey(name: 'totao_other', defaultValue: 0) required int totalOther,
   }) = _PGNUser;
 
   factory PGNUser.fromJson(Map<String, Object?> json) =>
@@ -18,7 +19,8 @@ class PGNUser with _$PGNUser {
 }
 
 const PGNUser defaultPGNUser = PGNUser(
-  userId: '',
+  id: '',
+  slackId: '',
   total: 0,
   totalPgrit: 0,
   totalDawn: 0,
