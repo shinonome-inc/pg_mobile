4482d944d75fc756e909deafb7287fe46a53936f
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-03-02 14:48:33 +0900

    ImagePathsクラスを作成してPGN画像のパスを追加

---
+++ lib/constants/image_paths.dart
@@ -0,0 +1,12 @@
+class ImagePaths {
+  ImagePaths._();
+
+  static const String pgnBronze = 'assets/images/pgnBronze.png';
+  static const String pgnDiamond = 'assets/images/pgnDiamond.png';
+  static const String pgnGold = 'assets/images/pgnGold.png';
+  static const String pgnGrandMaster = 'assets/images/pgnGrandMaster.png';
+  static const String pgnIron = 'assets/images/pgnIron.png';
+  static const String pgnMaster = 'assets/images/pgnMaster.png';
+  static const String pgnPlatinum = 'assets/images/pgnPlatinum.png';
+  static const String pgnSilver = 'assets/images/pgnSilver.png';
+}
