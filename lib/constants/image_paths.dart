5e56fe157f5adceb8e751718f38cb4742e2818c2
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-03-03 12:14:17 +0900

    PGNのランク画像をassets/images/ranksに移動

--- lib/constants/image_paths.dart
+++ lib/constants/image_paths.dart
@@ -1,12 +1,12 @@
 class ImagePaths {
   ImagePaths._();
 
-  static const String pgnBronze = 'assets/images/pgnBronze.png';
-  static const String pgnDiamond = 'assets/images/pgnDiamond.png';
-  static const String pgnGold = 'assets/images/pgnGold.png';
-  static const String pgnGrandMaster = 'assets/images/pgnGrandMaster.png';
-  static const String pgnIron = 'assets/images/pgnIron.png';
-  static const String pgnMaster = 'assets/images/pgnMaster.png';
-  static const String pgnPlatinum = 'assets/images/pgnPlatinum.png';
-  static const String pgnSilver = 'assets/images/pgnSilver.png';
+  static const String pgnBronze = 'assets/images/ranks/pgnBronze.png';
+  static const String pgnDiamond = 'assets/images/ranks/pgnDiamond.png';
+  static const String pgnGold = 'assets/images/ranks/pgnGold.png';
+  static const String pgnGrandMaster = 'assets/ranks/images/pgnGrandMaster.png';
+  static const String pgnIron = 'assets/images/ranks/pgnIron.png';
+  static const String pgnMaster = 'assets/images/ranks/pgnMaster.png';
+  static const String pgnPlatinum = 'assets/images/ranks/pgnPlatinum.png';
+  static const String pgnSilver = 'assets/images/ranks/pgnSilver.png';
 }
