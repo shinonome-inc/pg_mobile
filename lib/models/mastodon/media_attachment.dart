7f05e43246cfc82e751c82f244dc5bc2188e6f44
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-04-07 16:42:28 +0900

    MediaAttachmentクラスを追加

---
+++ lib/models/mastodon/media_attachment.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'media_attachment.freezed.dart';
+part 'media_attachment.g.dart';
+
+@freezed
+abstract class MediaAttachment with _$MediaAttachment {
+  @JsonSerializable(fieldRename: FieldRename.snake)
+  const factory MediaAttachment({
+    required String id,
+    required String type,
+    required String url,
+    required String previewUrl,
+    String? remoteUrl,
+    required dynamic meta,
+    String? description,
+    required String blurhash,
+  }) = _MediaAttachment;
+
+  factory MediaAttachment.fromJson(Map<String, dynamic> json) =>
+      _$MediaAttachmentFromJson(json);
+}
