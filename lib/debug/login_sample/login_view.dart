fbc9eb44a1bf387ce12137036f6eb14de5f47ae1
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-04-20 14:39:14 +0900

    LoginViewクラスのインデントを修正

--- lib/debug/login_sample/login_view.dart
+++ lib/debug/login_sample/login_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pg_mobile/debug/debug_page.dart';
 import 'package:pg_mobile/repository/mastodon_repository.dart';
-import 'package:pg_mobile/repository/secure_storage_repository.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class LoginView extends StatefulWidget {
@@ -18,22 +17,23 @@ class _LoginViewState extends State<LoginView> {
     super.initState();
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(
-          NavigationDelegate(onPageFinished: (String uri) async {
-        final url = Uri.parse(uri);
-        if (url.queryParameters['code'] != null) {
-          final accessToken = await MastodonRepository.instance.signIn(url);
-          if (accessToken != null) {
-            await MastodonRepository.instance.set(accessToken);
-            await SecureStorageRepository.writeToken(accessToken);
-            if (!mounted) return;
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (_) => const DebugPage()),
-            );
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageFinished: (String url) async {
+          final uri = Uri.parse(url);
+          if (uri.queryParameters['code'] == null) {
+            return;
           }
-        }
-      }))
+          final accessToken = await MastodonRepository.instance.signIn(uri);
+          if (accessToken == null) {
+            return;
+          }
+          if (!mounted) return;
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => const DebugPage()),
+          );
+        },
+      ))
       ..loadRequest(Uri.parse(MastodonRepository.instance.url));
   }
 
