74d48b0b66a3a2c7884f9109716db8a07ef44545
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-04-20 15:36:50 +0900

    MastodonRepositoryクラスにfetchCredentialAccountメソッドを追加

--- lib/repository/mastodon_repository.dart
+++ lib/repository/mastodon_repository.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:pg_mobile/config/env.dart';
 import 'package:pg_mobile/models/mastodon/account.dart';
+import 'package:pg_mobile/models/mastodon/credential_account.dart';
 import 'package:pg_mobile/models/mastodon/status.dart';
 import 'package:pg_mobile/repository/secure_storage_repository.dart';
 
@@ -115,6 +116,18 @@ class MastodonRepository {
     } else {
       throw Exception(
         'Failed to fetch user with status code ${response.statusCode}',
+      );
+    }
+  }
+
+  Future<CredentialAccount> fetchCredentialAccount() async {
+    final response = await _dio.get('/api/v1/accounts/verify_credentials');
+    if (response.statusCode == 200) {
+      final credentialAccount = CredentialAccount.fromJson(response.data);
+      return credentialAccount;
+    } else {
+      throw Exception(
+        'Failed to fetch credential account. response status code is ${response.statusCode}. error message is ${response.statusMessage}.',
       );
     }
   }
