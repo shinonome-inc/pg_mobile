d1fd82b37ca8b1ec40d02110d56ced1c0626bdbb
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-04-21 22:03:05 +0900

    Patternsクラスの正規表現を修正

--- lib/constants/patterns.dart
+++ lib/constants/patterns.dart
@@ -2,7 +2,7 @@ class Patterns {
   Patterns._();
 
   static final RegExp url =
-      RegExp(r"[\n\s]+https?://[\w!?/+\-_~;.,*&@#$%()'[\]]+");
-  static final RegExp mention = RegExp(r'[\n\s]+@(\w+)');
+      RegExp(r"(?:^|[\n\s])https?://[\w!?/+\-_~;.,*&@#$%()'[\]]+");
+  static final RegExp mention = RegExp(r'(?:^|[\n\s])@(\w+)');
   static final RegExp hashtagPattern = RegExp(r'#[0-9a-zA-Zぁ-んァ-ヶｱ-ﾝﾞﾟ一-龠]+');
 }
