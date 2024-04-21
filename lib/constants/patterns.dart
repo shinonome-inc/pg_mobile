6a017b5e946d34760b7dca2bb1af623111903c26
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-04-21 19:30:11 +0900

    Patternsクラスを修正

--- lib/constants/patterns.dart
+++ lib/constants/patterns.dart
@@ -1,7 +1,8 @@
 class Patterns {
   Patterns._();
 
-  static final RegExp url = RegExp(r" https?://[\w!?/+\-_~;.,*&@#$%()'[\]]+");
-  static final RegExp mention = RegExp(r' @(\w+)');
+  static final RegExp url =
+      RegExp(r"[\n\s]+https?://[\w!?/+\-_~;.,*&@#$%()'[\]]+");
+  static final RegExp mention = RegExp(r'[\n\s]+@(\w+)');
   static final RegExp hashtagPattern = RegExp(r'#[0-9a-zA-Zぁ-んァ-ヶｱ-ﾝﾞﾟ一-龠]+');
 }
