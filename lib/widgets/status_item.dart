eefe49fa616fab336b0b61b88b83536c4270cce5
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-05-11 15:30:41 +0900

    StatusItemクラスをリファクタリング

--- lib/widgets/status_item.dart
+++ lib/widgets/status_item.dart
@@ -31,114 +31,105 @@ class StatusItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 16, top: 16, right: 16),
-      child: Column(
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 16.h, horizontal: 16.w),
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: AppColors.gray2),
+        ),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              NetworkImageContainer(
-                imageUrl: status.account.avatar,
-                width: 56.w,
-                height: 56.h,
-                boxShape: BoxShape.circle,
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+          NetworkImageContainer(
+            imageUrl: status.account.avatar,
+            width: 56.w,
+            height: 56.w,
+            boxShape: BoxShape.circle,
+          ),
+          SizedBox(width: 8.w),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Row(
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                status.account.displayName,
-                                style: Theme.of(context).textTheme.bodyMedium,
-                              ),
-                              Text(
-                                '@${status.account.username}',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyMedium!
-                                    .copyWith(color: AppColors.gray3),
-                              ),
-                            ],
-                          ),
-                          const Spacer(),
-                          Text(
-                            status.createdAtText,
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium!
-                                .copyWith(color: AppColors.gray3),
-                          ),
-                        ],
+                    Text(
+                      status.account.displayName,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                    Expanded(
+                      child: Text(
+                        '@${status.account.username}',
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium!
+                            .copyWith(color: AppColors.gray3),
                       ),
                     ),
-                    LinkableText(
-                      status.contentText,
-                      onTapMention: (value) {
-                        // TODO: ユーザー画面へ遷移する。
-                        debugPrint('on tap mention: $value');
-                      },
-                      onTapHashtag: (value) {
-                        // TODO: ハッシュタグ画面へ遷移する。
-                        debugPrint('on tap hashtag: $value');
-                      },
-                    ),
-                    Row(
-                      children: [
-                        SizedBox(width: 8.w),
-                        StatusFooterItem(
-                          onTap: () {},
-                          iconData: Icons.reply,
-                          count: status.repliesCount,
-                          color: AppColors.gray3,
-                        ),
-                        const Spacer(),
-                        StatusFooterItem(
-                          onTap: () => _onTapBoost(status),
-                          iconData: Icons.repeat,
-                          count: status.reblogsCount,
-                          color: status.reblogged!
-                              ? AppColors.blue
-                              : AppColors.gray3,
-                        ),
-                        const Spacer(),
-                        StatusFooterItem(
-                          onTap: () => _onTapFavorite(status),
-                          iconData: status.favourited!
-                              ? Icons.star
-                              : Icons.star_border,
-                          count: status.favouritesCount,
-                          color: status.favourited!
-                              ? AppColors.yellow
-                              : AppColors.gray3,
-                        ),
-                        const Spacer(),
-                        SizedBox(
-                          width: 24.w,
-                          child: const Icon(Icons.more_horiz,
-                              color: AppColors.gray3),
-                        ),
-                        const Spacer(),
-                      ],
+                    const Spacer(),
+                    Text(
+                      status.createdAtText,
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyMedium!
+                          .copyWith(color: AppColors.gray3),
                     ),
                   ],
                 ),
-              )
-            ],
+                SizedBox(height: 8.h),
+                LinkableText(
+                  status.contentText,
+                  onTapMention: (value) {
+                    // TODO: ユーザー画面へ遷移する。
+                    debugPrint('on tap mention: $value');
+                  },
+                  onTapHashtag: (value) {
+                    // TODO: ハッシュタグ画面へ遷移する。
+                    debugPrint('on tap hashtag: $value');
+                  },
+                ),
+                SizedBox(height: 8.h),
+                Row(
+                  children: [
+                    StatusFooterItem(
+                      onTap: () {},
+                      iconData: Icons.reply,
+                      count: status.repliesCount,
+                      color: AppColors.gray3,
+                    ),
+                    const Spacer(),
+                    StatusFooterItem(
+                      onTap: () => _onTapBoost(status),
+                      iconData: Icons.repeat,
+                      count: status.reblogsCount,
+                      color:
+                          status.reblogged! ? AppColors.blue : AppColors.gray3,
+                    ),
+                    const Spacer(),
+                    StatusFooterItem(
+                      onTap: () => _onTapFavorite(status),
+                      iconData:
+                          status.favourited! ? Icons.star : Icons.star_border,
+                      count: status.favouritesCount,
+                      color: status.favourited!
+                          ? AppColors.yellow
+                          : AppColors.gray3,
+                    ),
+                    const Spacer(),
+                    SizedBox(
+                      width: 24.w,
+                      child:
+                          const Icon(Icons.more_horiz, color: AppColors.gray3),
+                    ),
+                    const Spacer(),
+                  ],
+                ),
+              ],
+            ),
           ),
-          SizedBox(height: 16.h),
-          const Divider(
-            thickness: 1,
-            color: AppColors.gray2,
-            height: 0,
-          )
         ],
       ),
     );
