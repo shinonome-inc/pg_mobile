2dc45ae3ac03eee8bd225ed54ad1883672570757
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-05-12 23:57:09 +0900

    DebugHomeTimelinePageのUIを変更

--- lib/debug/debug_home_timeline_page.dart
+++ lib/debug/debug_home_timeline_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pg_mobile/providers/timeline_notifier.dart';
-import 'package:pg_mobile/widgets/search_bar_widget.dart';
 import 'package:pg_mobile/widgets/status_view.dart';
 
 class DebugHomeTimelinePage extends ConsumerStatefulWidget {
@@ -14,6 +13,10 @@ class DebugHomeTimelinePage extends ConsumerStatefulWidget {
 
 class _DebugHomeTimelinePageState extends ConsumerState<DebugHomeTimelinePage> {
   late final ScrollController _scrollController;
+
+  void _onPressedNewPost() {
+    // TODO: 新規投稿機能を追加する。
+  }
 
   // スクロールして一番下に行ったら、次のページの分のStatusを取得して表示
   @override
@@ -28,22 +31,13 @@ class _DebugHomeTimelinePageState extends ConsumerState<DebugHomeTimelinePage> {
     super.initState();
   }
 
-  Future<void> _onRefresh() async {
-    final notifier = ref.read(timelineProvider.notifier);
-    notifier.reset();
-    notifier.fetchTimeline();
-  }
-
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(timelineProvider);
+    final notifier = ref.read(timelineProvider.notifier);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
-        bottom: const PreferredSize(
-          preferredSize: Size.fromHeight(48),
-          child: SearchBarWidget(),
-        ),
       ),
       body: state.isLoading && state.statuses.isEmpty
           ? const Center(
@@ -52,8 +46,12 @@ class _DebugHomeTimelinePageState extends ConsumerState<DebugHomeTimelinePage> {
           : StatusView(
               statuses: state.statuses,
               controller: _scrollController,
-              onRefresh: _onRefresh,
+              onRefresh: notifier.onRefresh,
             ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _onPressedNewPost,
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
