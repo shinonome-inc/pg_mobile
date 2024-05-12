5f81b61fdf4bbdd3c47fb8a89cb4282dba68e42f
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-05-12 22:47:25 +0900

    FlickVideoPlayerを用いてNetworkVideoPreviewのUIを構築

--- lib/widgets/network_video_preview.dart
+++ lib/widgets/network_video_preview.dart
@@ -1,5 +1,5 @@
+import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/material.dart';
-import 'package:pg_mobile/constants/app_colors.dart';
 import 'package:video_player/video_player.dart';
 
 class NetworkVideoPreview extends StatefulWidget {
@@ -15,48 +15,23 @@ class NetworkVideoPreview extends StatefulWidget {
 }
 
 class _NetworkVideoPreviewState extends State<NetworkVideoPreview> {
-  bool _isLoading = false;
-  bool _isPlaying = true;
+  late FlickManager _flickManager;
 
-  Future<void> _onPressedPlayButton() async {
-    if (_isLoading) return;
-    setState(() {
-      _isLoading = true;
-    });
-    try {
-      if (_isPlaying) {
-        await widget.controller.pause();
-      } else {
-        await widget.controller.play();
-      }
-    } catch (e) {
-      throw Exception('Failed to play or pause video: $e');
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
-    }
-    setState(() {
-      _isPlaying = !_isPlaying;
-    });
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.setLooping(false);
+    _flickManager = FlickManager(
+      videoPlayerController: widget.controller,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Stack(
-          alignment: Alignment.center,
-          children: [
-            VideoPlayer(
-              widget.controller,
-            ),
-            IconButton(
-              onPressed: _onPressedPlayButton,
-              icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
-              color: AppColors.white,
-            ),
-          ],
+        FlickVideoPlayer(
+          flickManager: _flickManager,
         ),
         const SafeArea(
           child: CloseButton(),
