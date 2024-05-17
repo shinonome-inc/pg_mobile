817af0e0b096c1ac775d3364fb40c8fc9ea98455
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-05-17 09:17:29 +0900

    StatusMenuActionTypeを追加

--- lib/models/mastodon/status_menu_action.dart
+++ lib/models/mastodon/status_menu_action.dart
@@ -2,11 +2,27 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'status_menu_action.freezed.dart';
 
+enum StatusMenuActionType {
+  common,
+  onlySignedInUser,
+  onlyNotSignedInUser,
+  cancel;
+}
+
 @freezed
 class StatusMenuAction with _$StatusMenuAction {
   const factory StatusMenuAction({
     required void Function() onPressed,
     required String text,
-    @Default(false) bool isCancel,
+    required StatusMenuActionType type,
   }) = _StatusMenuAction;
+}
+
+extension StatusMenuActionExtension on StatusMenuAction {
+  bool get isCommon => type == StatusMenuActionType.common;
+  bool get isOnlySignedInUser => type == StatusMenuActionType.onlySignedInUser;
+  bool get isOnlyNotSignedInUser =>
+      type == StatusMenuActionType.onlyNotSignedInUser;
+  bool get isCancel => type == StatusMenuActionType.cancel;
+  bool get isNotCancel => type != StatusMenuActionType.cancel;
 }
