5d6dec25d2ff38a133e919e886588ff6e9d94571
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-05-18 11:10:56 +0900

    投稿をタップで詳細が表示されるように修正

--- lib/widgets/status_item.dart
+++ lib/widgets/status_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pg_mobile/constants/app_colors.dart';
+import 'package:pg_mobile/debug/debug_thread_page.dart';
 import 'package:pg_mobile/models/mastodon/account.dart';
 import 'package:pg_mobile/models/mastodon/status.dart';
 import 'package:pg_mobile/models/mastodon/status_menu_action.dart';
@@ -126,152 +127,160 @@ class _StatusItemState extends ConsumerState<StatusItem> {
         isDestructiveAction: true,
       ),
     ];
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 16.h, horizontal: 16.w),
-      decoration: const BoxDecoration(
-        border: Border(
-          bottom: BorderSide(color: AppColors.gray2),
+    return GestureDetector(
+      onTap: () {
+        NavigatorUtil.pushScreen(
+          context,
+          DebugThreadPage(selectedStatus: widget.status),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 16.h, horizontal: 16.w),
+        decoration: const BoxDecoration(
+          border: Border(
+            bottom: BorderSide(color: AppColors.gray2),
+          ),
         ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          if (widget.reblogAccount != null)
-            Column(
-              children: [
-                Row(
-                  children: [
-                    SizedBox(width: 40.w),
-                    const Icon(
-                      Icons.repeat,
-                      color: AppColors.gray3,
-                    ),
-                    Text(
-                      '${widget.reblogAccount!.username}さんがブースト',
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyMedium!
-                          .copyWith(color: AppColors.gray3),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 8.h),
-              ],
-            ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              NetworkImageContainer(
-                imageUrl: widget.status.account.avatar,
-                width: 56.w,
-                height: 56.w,
-                boxShape: BoxShape.circle,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (widget.reblogAccount != null)
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(width: 40.w),
+                      const Icon(
+                        Icons.repeat,
+                        color: AppColors.gray3,
+                      ),
+                      Text(
+                        '${widget.reblogAccount!.username}さんがブースト',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium!
+                            .copyWith(color: AppColors.gray3),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 8.h),
+                ],
               ),
-              SizedBox(width: 8.w),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          widget.status.account.displayName,
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                        SizedBox(width: 8.w),
-                        Expanded(
-                          child: Text(
-                            '@${widget.status.account.username}',
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                NetworkImageContainer(
+                  imageUrl: widget.status.account.avatar,
+                  width: 56.w,
+                  height: 56.w,
+                  boxShape: BoxShape.circle,
+                ),
+                SizedBox(width: 8.w),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            widget.status.account.displayName,
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          SizedBox(width: 8.w),
+                          Expanded(
+                            child: Text(
+                              '@${widget.status.account.username}',
+                              overflow: TextOverflow.ellipsis,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .copyWith(color: AppColors.gray3),
+                            ),
+                          ),
+                          Text(
+                            widget.status.createdAtText,
                             overflow: TextOverflow.ellipsis,
                             style: Theme.of(context)
                                 .textTheme
                                 .bodyMedium!
                                 .copyWith(color: AppColors.gray3),
                           ),
-                        ),
-                        Text(
-                          widget.status.createdAtText,
-                          overflow: TextOverflow.ellipsis,
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium!
-                              .copyWith(color: AppColors.gray3),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 8.h),
-                    LinkableText(
-                      widget.status.contentText,
-                      onTapMention: (value) {
-                        // TODO: ユーザー画面へ遷移する。
-                        debugPrint('on tap mention: $value');
-                      },
-                      onTapHashtag: (value) {
-                        // TODO: ハッシュタグ画面へ遷移する。
-                        debugPrint('on tap hashtag: $value');
-                      },
-                    ),
-                    SizedBox(height: 8.h),
-                    if (widget.status.mediaAttachments.isNotEmpty) ...{
-                      SizedBox(
-                        height: 160.h,
-                        child: StatusMediaView(
-                          mediaAttachments: widget.status.mediaAttachments,
-                        ),
+                        ],
                       ),
                       SizedBox(height: 8.h),
-                    },
-                    if (widget.status.showLinkPreview) ...{
-                      StatusLinkPreview(
-                        url: widget.status.urls.first,
+                      LinkableText(
+                        widget.status.contentText,
+                        onTapMention: (value) {
+                          // TODO: ユーザー画面へ遷移する。
+                          debugPrint('on tap mention: $value');
+                        },
+                        onTapHashtag: (value) {
+                          // TODO: ハッシュタグ画面へ遷移する。
+                          debugPrint('on tap hashtag: $value');
+                        },
                       ),
                       SizedBox(height: 8.h),
-                    },
-                    Row(
-                      children: [
-                        StatusFooterItem(
-                          onTap: () => _onTapReply(widget.status),
-                          iconData: Icons.reply,
-                          count: widget.status.repliesCount,
-                          color: AppColors.gray3,
-                        ),
-                        const Spacer(),
-                        StatusFooterItem(
-                          onTap: () => notifier.onTapBoost(widget.status),
-                          iconData: Icons.repeat,
-                          count: widget.status.reblogsCount,
-                          color: widget.status.reblogged!
-                              ? AppColors.blue
-                              : AppColors.gray3,
-                        ),
-                        const Spacer(),
-                        StatusFooterItem(
-                          onTap: () => notifier.onTapFavorite(widget.status),
-                          iconData: widget.status.favourited!
-                              ? Icons.star
-                              : Icons.star_border,
-                          count: widget.status.favouritesCount,
-                          color: widget.status.favourited!
-                              ? AppColors.yellow
-                              : AppColors.gray3,
-                        ),
-                        const Spacer(),
-                        GestureDetector(
-                          onTap: () => _onTapMenu(statusMenuActions),
-                          child: const Icon(
-                            Icons.more_horiz,
-                            color: AppColors.gray3,
+                      if (widget.status.mediaAttachments.isNotEmpty) ...{
+                        SizedBox(
+                          height: 160.h,
+                          child: StatusMediaView(
+                            mediaAttachments: widget.status.mediaAttachments,
                           ),
                         ),
-                        const Spacer(),
-                      ],
-                    ),
-                  ],
+                        SizedBox(height: 8.h),
+                      },
+                      if (widget.status.showLinkPreview) ...{
+                        StatusLinkPreview(
+                          url: widget.status.urls.first,
+                        ),
+                        SizedBox(height: 8.h),
+                      },
+                      Row(
+                        children: [
+                          StatusFooterItem(
+                            onTap: () => _onTapReply(widget.status),
+                            iconData: Icons.reply,
+                            count: widget.status.repliesCount,
+                            color: AppColors.gray3,
+                          ),
+                          const Spacer(),
+                          StatusFooterItem(
+                            onTap: () => notifier.onTapBoost(widget.status),
+                            iconData: Icons.repeat,
+                            count: widget.status.reblogsCount,
+                            color: widget.status.reblogged!
+                                ? AppColors.blue
+                                : AppColors.gray3,
+                          ),
+                          const Spacer(),
+                          StatusFooterItem(
+                            onTap: () => notifier.onTapFavorite(widget.status),
+                            iconData: widget.status.favourited!
+                                ? Icons.star
+                                : Icons.star_border,
+                            count: widget.status.favouritesCount,
+                            color: widget.status.favourited!
+                                ? AppColors.yellow
+                                : AppColors.gray3,
+                          ),
+                          const Spacer(),
+                          GestureDetector(
+                            onTap: () => _onTapMenu(statusMenuActions),
+                            child: const Icon(
+                              Icons.more_horiz,
+                              color: AppColors.gray3,
+                            ),
+                          ),
+                          const Spacer(),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
