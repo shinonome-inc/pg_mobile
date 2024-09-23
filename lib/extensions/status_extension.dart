67f10b3626e0323bafd96e999d5cd592070aac0c
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-09-23 18:08:47 +0900

    extensionsのロジックをutilsに移植

--- lib/extensions/status_extension.dart
+++ lib/extensions/status_extension.dart
@@ -1,42 +1,17 @@
-import 'package:html/parser.dart';
-import 'package:pg_mobile/constants/patterns.dart';
 import 'package:pg_mobile/models/mastodon/status.dart';
 import 'package:pg_mobile/util/date_formatter.dart';
+import 'package:pg_mobile/util/status_util.dart';
 
 extension StatusExtension on Status {
   DateTime get _createdAt => DateTime.parse(createdAt).toLocal();
-
   String get createdAtText => DateFormatter.formatStatusDetail(_createdAt);
-
   String get createdAtTimeAgoText =>
       DateFormatter.formatTimeAgoDate(_createdAt);
 
-  String get contentText {
-    return parse(
-      content.replaceAll('<br />', '\n').replaceAll('</p><p>', '\n\n'),
-    ).body!.text;
-  }
-
-  List<String> get urls {
-    List<String> urls = [];
-    final regExp = Patterns.url;
-    final matches = regExp.allMatches(contentText);
-    for (var regExpMatch in matches) {
-      final url = contentText.substring(regExpMatch.start, regExpMatch.end);
-      urls.add(url);
-    }
-    return urls;
-  }
-
-  String get mentionsText {
-    String mentionsText = '';
-    for (var mention in mentions) {
-      mentionsText += '@${mention.username} ';
-    }
-    return mentionsText;
-  }
+  String get contentText => StatusUtil.convertHtmlToPlainText(content);
+  List<String> get urls => StatusUtil.extractUrls(contentText);
+  String get mentionsText => StatusUtil.extractMentionsText(mentions);
 
   bool get containsUrl => urls.isNotEmpty;
-
   bool get showLinkPreview => containsUrl && mediaAttachments.isEmpty;
 }
---
+++ lib/util/status_util.dart
@@ -0,0 +1,36 @@
+import 'package:html/parser.dart';
+import 'package:pg_mobile/constants/patterns.dart';
+import 'package:pg_mobile/models/mastodon/status_mention.dart';
+
+/// ステータスに関するユーティリティクラスです。
+class StatusUtil {
+  StatusUtil._();
+
+  /// HTMLテキストを解析してテキストに変換する。
+  static String convertHtmlToPlainText(String htmlText) {
+    return parse(
+      htmlText.replaceAll('<br />', '\n').replaceAll('</p><p>', '\n\n'),
+    ).body!.text;
+  }
+
+  /// contentText内のURLを抽出する。
+  static List<String> extractUrls(String contentText) {
+    List<String> urls = [];
+    final regExp = Patterns.url;
+    final matches = regExp.allMatches(contentText);
+    for (var regExpMatch in matches) {
+      final url = contentText.substring(regExpMatch.start, regExpMatch.end);
+      urls.add(url);
+    }
+    return urls;
+  }
+
+  /// メンションされたユーザー名を '@username' の形式で抽出
+  static String extractMentionsText(List<StatusMention> mentions) {
+    String mentionsText = '';
+    for (var mention in mentions) {
+      mentionsText += '@${mention.username} ';
+    }
+    return mentionsText;
+  }
+}
