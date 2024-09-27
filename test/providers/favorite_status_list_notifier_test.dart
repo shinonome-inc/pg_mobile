f6738bc25b3ad8461deb13011554865fad7cc016
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-09-27 23:53:30 +0900

    FavoriteStatusListNotifierのテストコードを追加

---
+++ test/providers/favorite_status_list_notifier_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pg_mobile/models/mastodon/account.dart';
+import 'package:pg_mobile/providers/favorite_status_list_notifier.dart';
+
+import '../provider_container.dart';
+
+void main() {
+  group('FavoriteStatusListNotifier', () {
+    test('build', () {
+      final container = createContainer();
+      final notifier =
+          container.read(favoriteStatusListNotifierProvider.notifier);
+
+      expect(notifier.state, initialAccount);
+    });
+  });
+}
