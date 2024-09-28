20322450b3bf97c0a3b7d4269fe1cab88f03f30f
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-09-28 14:10:21 +0900

    ブースト及びブースト取り消しが正しくUIに反映されるように修正

--- lib/providers/timeline_notifier.dart
+++ lib/providers/timeline_notifier.dart
@@ -85,29 +85,29 @@ class TimelineNotifier extends _$TimelineNotifier {
     _addStatus(postedStatus);
   }
 
-  Future<void> onTapBoost(Status tappedStatus) async {
-    if (tappedStatus.reblogged == null || state.isLoading) return;
+  Future<void> onTapBoost(Status status) async {
+    if (status.reblogged == null || state.isLoading) return;
     setLoading(true);
-    Status status;
-    if (tappedStatus.reblogged!) {
-      status = await MastodonRepository.instance.undoBoostStatus(
-        tappedStatus.id,
+    if (status.reblogged!) {
+      final undoBoostedStatus = status.copyWith(
+        reblogged: false,
+        reblogsCount: status.reblogsCount - 1,
       );
-      status = status.copyWith(reblogsCount: status.reblogsCount - 1);
-      print(
-        'reblogged: ${status.reblogged}, reblogsCount: ${status.reblogsCount}',
+      _setStatus(undoBoostedStatus);
+      await MastodonRepository.instance.undoBoostStatus(
+        status.id,
       );
     } else {
-      status = await MastodonRepository.instance.boostStatus(
-        tappedStatus.id,
+      final boostedStatus = status.copyWith(
+        reblogged: true,
+        reblogsCount: status.reblogsCount + 1,
       );
-      status = status.copyWith(reblogsCount: status.reblogsCount + 1);
-      print(
-        'reblogged: ${status.reblogged}, reblogsCount: ${status.reblogsCount}',
+      _setStatus(boostedStatus);
+      await MastodonRepository.instance.boostStatus(
+        status.id,
       );
     }
     setLoading(false);
-    _setStatus(status);
   }
 
   Future<void> onTapFavorite(Status tappedStatus) async {
