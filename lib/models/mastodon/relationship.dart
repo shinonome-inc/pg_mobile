e5f5212bd49a6439a5ebb65b0a5151d451f8fad2
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-11-30 21:24:36 +0900

    Relationshipクラスを追加

---
+++ lib/models/mastodon/relationship.dart
@@ -0,0 +1,33 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'relationship.freezed.dart';
+part 'relationship.g.dart';
+
+@freezed
+
+/// Relationshipのモデルクラス。
+///
+/// Relationshipは、フォローやミュート、ブロックなどのアカウント間の関係を表す。
+class Relationship with _$Relationship {
+  @JsonSerializable(fieldRename: FieldRename.snake)
+  const factory Relationship({
+    required String id,
+    required bool following,
+    required bool showingReblogs,
+    required bool notifying,
+    List<String>? languages,
+    required bool followedBy,
+    required bool blocking,
+    required bool blockedBy,
+    required bool muting,
+    required bool mutingNotifications,
+    required bool requested,
+    required bool requestedBy,
+    required bool domainBlocking,
+    required bool endorsed,
+    required String note,
+  }) = _Relationship;
+
+  factory Relationship.fromJson(Map<String, dynamic> json) =>
+      _$RelationshipFromJson(json);
+}
