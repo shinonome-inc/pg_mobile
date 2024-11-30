15be09e17bb51a820825ee728459abc376324296
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-11-30 22:52:24 +0900

    StatusListPageにStatusItemを用いて投稿を表示

--- lib/pages/status_list/status_list_page.dart
+++ lib/pages/status_list/status_list_page.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-import 'package:pg_mobile/models/enums/app_page.dart';
+import 'package:pg_mobile/constants/app_colors.dart';
+import 'package:pg_mobile/extensions/status_extension.dart';
+import 'package:pg_mobile/models/mastodon/status.dart';
+import 'package:pg_mobile/models/mastodon/status_menu_action.dart';
+import 'package:pg_mobile/providers/signed_in_user_notifier.dart';
+import 'package:pg_mobile/providers/timeline_notifier.dart';
+import 'package:pg_mobile/util/navigator_util.dart';
+import 'package:pg_mobile/util/status_menu_action_util.dart';
+import 'package:pg_mobile/widgets/status_item.dart';
 
 class StatusListPage extends ConsumerStatefulWidget {
   const StatusListPage({super.key});
@@ -11,50 +18,158 @@ class StatusListPage extends ConsumerStatefulWidget {
 }
 
 class _StatusListPageState extends ConsumerState<StatusListPage> {
+  final ScrollController _controller = ScrollController();
+
+  void _onTapHashtag(String hashtag) {
+    // TODO: ハッシュタグをタップ
+  }
+
+  void _onTapMention(String hashtag) {
+    // TODO: メンションをタップ
+  }
+
+  void _onTapReply(Status tappedStatus) {
+    NavigatorUtil.showNewPostCreateView(
+      context,
+      replyToStatus: tappedStatus,
+    );
+  }
+
+  Future<void> _onTapBoost(Status status) async {
+    final notifier = ref.read(timelineNotifierProvider.notifier);
+    await notifier.onTapBoost(status);
+  }
+
+  Future<void> _onTapFavorite(Status status) async {
+    final notifier = ref.read(timelineNotifierProvider.notifier);
+    await notifier.onTapFavorite(status);
+  }
+
+  void _copyLink() {
+    // TODO: リンクをコピー
+    NavigatorUtil.popScreen(context);
+  }
+
+  Future<void> _pinToProfile(Status status) async {
+    final notifier = ref.read(timelineNotifierProvider.notifier);
+    await notifier.pinStatusToProfile(status);
+    if (!mounted) return;
+    NavigatorUtil.popScreen(context);
+  }
+
+  Future<void> _unpinToProfile(Status status) async {
+    final notifier = ref.read(timelineNotifierProvider.notifier);
+    await notifier.unpinStatusToProfile(status);
+    if (!mounted) return;
+    NavigatorUtil.popScreen(context);
+  }
+
+  void _deleteAndReturnToDraft() {
+    // TODO: 削除して下書きに戻す
+    NavigatorUtil.popScreen(context);
+  }
+
+  Future<void> _delete(Status status) async {
+    final notifier = ref.read(timelineNotifierProvider.notifier);
+    await notifier.deleteStatus(status);
+    if (!mounted) return;
+    NavigatorUtil.popScreen(context);
+  }
+
+  Future<void> _mute(Status status) async {
+    final notifier = ref.read(timelineNotifierProvider.notifier);
+    await notifier.muteAccount(status.account);
+    if (!mounted) return;
+    NavigatorUtil.popScreen(context);
+  }
+
+  void _block() {
+    // TODO: ブロック
+    NavigatorUtil.popScreen(context);
+  }
+
+  void _cancel() {
+    NavigatorUtil.popScreen(context);
+  }
+
+  void _onTapEngagementReblog() {
+    // TODO: ブーストを押したユーザー一覧を表示する画面へ遷移
+  }
+
+  void _onTapEngagementFavorite() {
+    // TODO: お気に入りを押したユーザー一覧を表示する画面へ遷移
+  }
+
+  void _onTapMenu(List<StatusMenuAction> actions) {
+    NavigatorUtil.showStatusMenuActionSheet(context, actions: actions);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Future(() async {
+      await ref.read(timelineNotifierProvider.notifier).fetchTimeline();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final state = ref.watch(timelineNotifierProvider);
+    final notifier = ref.read(timelineNotifierProvider.notifier);
+    final signedInUser = ref.read(signedInUserNotifierProvider);
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          TextButton(
-            onPressed: () {
-              context.go(AppPage.user.path);
+      backgroundColor: AppColors.gray1,
+      body: RefreshIndicator(
+        onRefresh: notifier.onRefresh,
+        child: Scrollbar(
+          controller: _controller,
+          child: ListView.builder(
+            controller: _controller,
+            itemCount: state.statuses.length,
+            itemBuilder: (BuildContext context, int index) {
+              if (state.isLoading && state.statuses.isEmpty) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              final status = state.statuses[index];
+              final isSignedInUser = status.account.id == signedInUser.id;
+              final statusMenuActions =
+                  StatusMenuActionUtil.getStatusMenuActions(
+                status: status,
+                isSignedInUser: isSignedInUser,
+                onCopyLink: _copyLink,
+                onPinToProfile: () => status.isPinnedToProfile
+                    ? _unpinToProfile(status)
+                    : _pinToProfile(status),
+                onDeleteAndReturnToDraft: _deleteAndReturnToDraft,
+                onDelete: () => _delete(status),
+                onMute: () => _mute(status),
+                onBlock: () => _block(),
+                onCancel: _cancel,
+              );
+              return StatusItem(
+                status: status,
+                reblogAccount: status.reblog == null ? null : status.account,
+                onTapHashtag: () => _onTapHashtag,
+                onTapMention: () => _onTapMention,
+                onTapReply: () => _onTapReply(status),
+                onTapBoost: () => _onTapBoost(status),
+                onTapFavorite: () => _onTapFavorite(status),
+                onTapMenu: () => _onTapMenu(statusMenuActions),
+                onCopyLink: _copyLink,
+                onPinToProfile: () => isSignedInUser ? _pinToProfile : null,
+                onUnpinToProfile: () => isSignedInUser ? _unpinToProfile : null,
+                onDeleteAndReturnToDraft: () =>
+                    isSignedInUser ? _deleteAndReturnToDraft : null,
+                onDelete: () => isSignedInUser ? _delete : null,
+                onMute: () => _mute,
+                onBlock: _block,
+                onCancel: _cancel,
+              );
             },
-            child: const Text('User'),
           ),
-          TextButton(
-            onPressed: () {
-              context.go(AppPage.hashTag.path);
-            },
-            child: const Text('Hash Tag'),
-          ),
-          TextButton(
-            onPressed: () {
-              context.go(AppPage.user.path);
-            },
-            child: const Text('Status Detail'),
-          ),
-          TextButton(
-            onPressed: () {
-              context.go(AppPage.boostUserList.path);
-            },
-            child: const Text('Boost User List'),
-          ),
-          TextButton(
-            onPressed: () {
-              context.go(AppPage.favoriteUserList.path);
-            },
-            child: const Text('Favorite User List'),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          context.go(AppPage.createStatus.path);
-        },
+        ),
       ),
     );
   }
