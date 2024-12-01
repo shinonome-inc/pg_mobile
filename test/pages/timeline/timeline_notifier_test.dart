6dad4263b793727215daecfaa1816f036f06502f
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-12-01 10:09:00 +0900

    timeline_notifier_test.dartのパスを修正

--- test/pages/timeline/timeline_notifier_test.dart
+++ test/pages/timeline/timeline_notifier_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pg_mobile/pages/timeline/timeline_notifier.dart';
 import 'package:pg_mobile/pages/timeline/timeline_state.dart';
 
-import '../provider_container.dart';
+import '../../provider_container.dart';
 
 void main() {
   group('TimelineNotifier', () {
