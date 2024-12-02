61e261b245529b6c4ef1647924830a6e29b25a85
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-12-02 23:58:16 +0900

    SettingsSectionItemを追加

---
+++ lib/pages/settings/settings_section_item.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pg_mobile/constants/app_colors.dart';
+
+/// SettingsSectionItemTypeの種類を表す列挙型。
+enum SettingsSectionItemType {
+  normal,
+  top,
+  bottom,
+  single;
+
+  bool get _isTop => this == SettingsSectionItemType.top;
+  bool get _isBottom => this == SettingsSectionItemType.bottom;
+  bool get _isSingle => this == SettingsSectionItemType.single;
+
+  bool get _isTopRounded => _isTop || _isSingle;
+  bool get _isBottomRounded => _isBottom || _isSingle;
+}
+
+/// 設定画面のセクションのアイテムのWidget。
+///
+/// [type]の値に応じて、背景の角丸を変更する。
+///
+class SettingsSectionItem extends StatelessWidget {
+  const SettingsSectionItem({
+    super.key,
+    this.onTap,
+    required this.title,
+    required this.action,
+    this.type = SettingsSectionItemType.normal,
+  });
+
+  final void Function()? onTap;
+  final Widget title;
+  final Widget action;
+  final SettingsSectionItemType type;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Ink(
+        padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 16.w),
+        height: 48.h,
+        decoration: BoxDecoration(
+          color: AppColors.gray2,
+          borderRadius: BorderRadius.vertical(
+            top: type._isTopRounded ? Radius.circular(8.r) : Radius.zero,
+            bottom: type._isBottomRounded ? Radius.circular(8.r) : Radius.zero,
+          ),
+        ),
+        child: Row(
+          children: [
+            title,
+            const Spacer(),
+            action,
+          ],
+        ),
+      ),
+    );
+  }
+}
