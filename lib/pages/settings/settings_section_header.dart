2d2080d1e12480035bffa4c733fbaa25fb26a52f
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-12-04 22:17:29 +0900

    SettingsSectionHeaderのwidgetbook.UseCaseを追加

--- lib/pages/settings/settings_section_header.dart
+++ lib/pages/settings/settings_section_header.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pg_mobile/extensions/build_context_extension.dart';
+import 'package:pg_mobile/widgetbook.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 class SettingsSectionHeader extends StatelessWidget {
   const SettingsSectionHeader({
@@ -29,4 +32,21 @@ class SettingsSectionHeader extends StatelessWidget {
       ),
     );
   }
+}
+
+@widgetbook.UseCase(
+  name: 'SettingsSectionHeader',
+  type: SettingsSectionHeader,
+  path: '[widgets]/pages/settings',
+)
+Widget settingsSectionHeader(BuildContext context) {
+  return WidgetbookWrapper(
+    child: SettingsSectionHeader(
+      iconData: Icons.settings,
+      titleText: context.knobs.string(
+        label: 'Header text',
+        initialValue: 'テキスト',
+      ),
+    ),
+  );
 }
