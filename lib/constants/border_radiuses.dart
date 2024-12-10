eacca4281c0bb6978e9e729a0fc407aeb03c9ed3
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-12-10 20:42:06 +0900

    BorderRadiusesクラスを追加

---
+++ lib/constants/border_radiuses.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/rendering.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class BorderRadiuses {
+  BorderRadiuses._();
+
+  static BorderRadiusGeometry modalHeaderBorderRadius = BorderRadius.vertical(
+    top: Radius.circular(16.r),
+  );
+}
