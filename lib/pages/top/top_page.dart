9dcab27015b42d15d0d0036043e709707aa8de13
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-12-10 22:27:44 +0900

    WebViewUtilを追加

--- lib/pages/top/top_page.dart
+++ lib/pages/top/top_page.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:pg_mobile/extensions/build_context_extension.dart';
+import 'package:go_router/go_router.dart';
+import 'package:pg_mobile/constants/app_colors.dart';
+import 'package:pg_mobile/constants/border_radiuses.dart';
+import 'package:pg_mobile/models/enums/app_page.dart';
+import 'package:pg_mobile/pages/top/top_view.dart';
+import 'package:pg_mobile/repository/mastodon_repository.dart';
+import 'package:pg_mobile/widgets/scrollable_modal_bottom_sheet.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class TopPage extends ConsumerStatefulWidget {
   const TopPage({super.key});
@@ -11,34 +17,67 @@ class TopPage extends ConsumerStatefulWidget {
 }
 
 class _TopPageState extends ConsumerState<TopPage> {
+  late WebViewController _controller;
+  double height = 640;
+
+  void _onTapSignIn() async {
+    await showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadiuses.modalHeaderBorderRadius,
+      ),
+      backgroundColor: AppColors.transparent,
+      builder: (context) {
+        return ScrollableModalBottomSheet(
+          physics: const NeverScrollableScrollPhysics(),
+          child: SizedBox(
+            height: height,
+            child: WebViewWidget(controller: _controller),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _controller = WebViewController()
+        ..setJavaScriptMode(JavaScriptMode.unrestricted)
+        ..setNavigationDelegate(
+          NavigationDelegate(
+            onPageFinished: (String url) async {
+              print('onPageFinished: $url');
+              const String javaScript =
+                  'document.documentElement.scrollHeight;';
+              final result =
+                  await _controller.runJavaScriptReturningResult(javaScript);
+              setState(() {
+                height = double.parse(result.toString());
+              });
+              print('height: $height');
+              final uri = Uri.parse(url);
+              if (uri.queryParameters['code'] == null) return;
+              final accessToken =
+                  await MastodonRepository.instance.obtainToken(uri);
+              print('accessToken: $accessToken');
+              if (accessToken == null) return;
+              if (!mounted) return;
+              context.pushReplacement(AppPage.timeline.path);
+            },
+          ),
+        )
+        ..loadRequest(Uri.parse(MastodonRepository.authorizeUrl));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.w),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Spacer(),
-            Text(
-              'PGritへ\nようこそ！',
-              style: context.textTheme.displaySmallBold,
-              textAlign: TextAlign.center,
-            ),
-            const Spacer(),
-            ElevatedButton(
-              onPressed: () {
-                // TODO: ログインする。
-              },
-              child: Text(
-                'ログイン',
-                style: context.textTheme.bodyLargeBold,
-              ),
-            ),
-            SizedBox(height: 40.h),
-          ],
-        ),
+      body: TopView(
+        onTapSignIn: _onTapSignIn,
       ),
     );
   }
