e61b856fafeffe9dac912aafae4f42de1457194f
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-12-11 23:23:33 +0900

    SignedInUserNotifierに_setSignedInUserとsignInを追加

--- lib/providers/signed_in_user_notifier.dart
+++ lib/providers/signed_in_user_notifier.dart
@@ -1,5 +1,6 @@
 import 'package:pg_mobile/models/mastodon/account.dart';
 import 'package:pg_mobile/repository/mastodon_repository.dart';
+import 'package:pg_mobile/repository/secure_storage_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'signed_in_user_notifier.g.dart';
@@ -7,8 +8,19 @@ part 'signed_in_user_notifier.g.dart';
 @riverpod
 class SignedInUserNotifier extends _$SignedInUserNotifier {
   @override
-  Account build() {
-    return initialAccount;
+  Account? build() {
+    return null;
+  }
+
+  void _setSignedInUser(Account user) {
+    state = user;
+  }
+
+  Future<void> signIn(String accessToken) async {
+    final user = await MastodonRepository.instance.fetchCredentialAccount();
+    _setSignedInUser(user);
+    MastodonRepository.instance.set(accessToken);
+    await SecureStorageRepository.writeToken(accessToken);
   }
 
   Future<void> fetchUser() async {
