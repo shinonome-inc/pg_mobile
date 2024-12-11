dce583959db1207dac5810e987b1ce470dbb34d5
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-12-12 00:00:48 +0900

    LaunchNotifierのisSignedInを修正

--- lib/pages/launch/launch_notifier.dart
+++ lib/pages/launch/launch_notifier.dart
@@ -1,4 +1,5 @@
 import 'package:pg_mobile/pages/launch/launch_state.dart';
+import 'package:pg_mobile/providers/signed_in_user_notifier.dart';
 import 'package:pg_mobile/repository/secure_storage_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -17,7 +18,12 @@ class LaunchNotifier extends _$LaunchNotifier {
 
   Future<bool> isSignedIn() async {
     final token = await SecureStorageRepository.readToken();
-    final isSignedIn = token != null && token.isNotEmpty;
-    return isSignedIn;
+    if (token == null) return false;
+    try {
+      await ref.read(signedInUserNotifierProvider.notifier).signIn(token);
+    } catch (e) {
+      return false;
+    }
+    return true;
   }
 }
