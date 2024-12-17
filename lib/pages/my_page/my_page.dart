d2e6af27b1c7249414b453f7e07531cfaec41668
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-12-18 01:30:47 +0900

    MyPageに投稿一覧を追加

--- lib/pages/my_page/my_page.dart
+++ lib/pages/my_page/my_page.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
-import 'package:pg_mobile/constants/app_colors.dart';
 import 'package:pg_mobile/extensions/build_context_extension.dart';
 import 'package:pg_mobile/models/enums/app_page.dart';
+import 'package:pg_mobile/pages/my_page/my_page_notifier.dart';
 import 'package:pg_mobile/providers/signed_in_user_notifier.dart';
-import 'package:pg_mobile/widgets/linkable_text.dart';
-import 'package:pg_mobile/widgets/network_image_container.dart';
+import 'package:pg_mobile/widgets/status/status.dart';
+import 'package:pg_mobile/widgets/user/user_profile_view.dart';
 
 enum _TabMenu {
   post,
@@ -34,9 +34,30 @@ class MyPage extends ConsumerStatefulWidget {
 }
 
 class _MyPageState extends ConsumerState<MyPage> {
+  void _onTapStatusCount() {
+    context.go(AppPage.timeline.path);
+  }
+
+  void _onTapFollowingCount() {
+    context.go(AppPage.followingList.path);
+  }
+
+  void _onTapFollowerCount() {
+    context.go(AppPage.followerList.path);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await ref.read(myPageNotifierProvider.notifier).fetchStatuses();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final user = ref.watch(signedInUserNotifierProvider);
+    final state = ref.watch(myPageNotifierProvider);
     if (user == null) {
       return const Center(
         child: CircularProgressIndicator(),
@@ -48,96 +69,53 @@ class _MyPageState extends ConsumerState<MyPage> {
         appBar: AppBar(
           title: const Text('My Page'),
         ),
-        body: Stack(
-          children: [
-            SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  NetworkImageContainer(
-                    imageUrl: user.header,
-                    aspectRatio: 375 / 200,
-                    fit: BoxFit.cover,
-                  ),
-                  SizedBox(height: 56.h),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          user.displayName,
-                          style: context.textTheme.titleLargeBold,
-                        ),
-                        SizedBox(height: 8.h),
-                        Text(
-                          '@${user.username}',
-                          style: context.textTheme.bodyMediumNormal?.copyWith(
-                            color: AppColors.gray4,
-                          ),
-                        ),
-                        SizedBox(height: 16.h),
-                        LinkableText(
-                          user.note,
-                          onTapMention: (value) {},
-                          onTapHashtag: (value) {},
-                        ),
-                        SizedBox(width: 16.h),
-                        Row(
-                          children: [
-                            _UserActivityCountItem(
-                              onTap: () {
-                                context.push(AppPage.timeline.path);
-                              },
-                              count: user.statusesCount,
-                              label: '投稿',
-                            ),
-                            SizedBox(width: 16.w),
-                            _UserActivityCountItem(
-                              onTap: () {
-                                context.push(AppPage.followingList.path);
-                              },
-                              count: user.statusesCount,
-                              label: 'フォロー',
-                            ),
-                            SizedBox(width: 16.w),
-                            _UserActivityCountItem(
-                              onTap: () {
-                                context.push(AppPage.followerList.path);
-                              },
-                              count: user.statusesCount,
-                              label: 'フォロワー',
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 16.h),
-                  TabBar(
-                    labelStyle: context.textTheme.bodyMediumBold,
-                    unselectedLabelStyle: context.textTheme.bodyMediumNormal,
-                    tabs: <Widget>[
-                      for (final menu in _TabMenu.values) Tab(text: menu._text),
-                    ],
-                  ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              UserProfileView(
+                user: user,
+                onTapStatusCount: _onTapStatusCount,
+                onTapFollowingCount: _onTapFollowingCount,
+                onTapFollowerCount: _onTapFollowerCount,
+              ),
+              SizedBox(height: 16.h),
+              TabBar(
+                labelStyle: context.textTheme.bodyMediumBold,
+                unselectedLabelStyle: context.textTheme.bodyMediumNormal,
+                tabs: <Widget>[
+                  for (final menu in _TabMenu.values) Tab(text: menu._text),
                 ],
               ),
-            ),
-            Positioned(
-              top: 160.h,
-              left: 16.0,
-              child: NetworkImageContainer(
-                imageUrl: user.avatar,
-                padding: EdgeInsets.all(4.h),
-                backgroundColor: AppColors.gray2,
-                width: 88.h,
-                height: 88.h,
-                boxShape: BoxShape.circle,
+              ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: state.statuses.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return StatusItem(
+                    status: state.statuses.elementAt(index),
+                    onTapItem: () {},
+                    onTapAccount: () {},
+                    onTapHashtag: () {},
+                    onTapMention: () {},
+                    onTapReply: () {},
+                    onTapBoost: () {},
+                    onTapFavorite: () {},
+                    onTapMenu: () {},
+                    onCopyLink: () {},
+                    onPinToProfile: () {},
+                    onUnpinToProfile: () {},
+                    onDeleteAndReturnToDraft: () {},
+                    onDelete: () {},
+                    onMute: () {},
+                    onBlock: () {},
+                    onCancel: () {},
+                  );
+                },
               ),
-            ),
-          ],
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
@@ -145,40 +123,6 @@ class _MyPageState extends ConsumerState<MyPage> {
             context.push(AppPage.createStatus.path);
           },
         ),
-      ),
-    );
-  }
-}
-
-class _UserActivityCountItem extends StatelessWidget {
-  const _UserActivityCountItem({
-    required this.onTap,
-    required this.count,
-    required this.label,
-  });
-
-  final void Function() onTap;
-  final int count;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Row(
-        children: [
-          Text(
-            count.toString(),
-            style: context.textTheme.bodyMediumBold,
-          ),
-          const SizedBox(width: 8),
-          Text(
-            label,
-            style: context.textTheme.bodySmallNormal?.copyWith(
-              color: AppColors.gray4,
-            ),
-          ),
-        ],
       ),
     );
   }
