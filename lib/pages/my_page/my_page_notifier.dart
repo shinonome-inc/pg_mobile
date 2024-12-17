9e633de7dc920ccb9d006b232a6df5ce0850f378
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-12-18 01:28:19 +0900

    MyHomePageNotifierを追加

---
+++ lib/pages/my_page/my_page_notifier.dart
@@ -0,0 +1,34 @@
+import 'package:pg_mobile/pages/my_page/my_page_state.dart';
+import 'package:pg_mobile/providers/signed_in_user_notifier.dart';
+import 'package:pg_mobile/repository/mastodon_repository.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'my_page_notifier.g.dart';
+
+@riverpod
+class MyPageNotifier extends _$MyPageNotifier {
+  @override
+  MyPageState build() {
+    return initialMyPageState;
+  }
+
+  void _setLoading(bool isLoading) {
+    state = state.copyWith(isLoading: isLoading);
+  }
+
+  Future<void> fetchStatuses() async {
+    if (state.isLoading) return;
+    final accountId = ref.read(signedInUserNotifierProvider)?.id;
+    if (accountId == null) return;
+    _setLoading(true);
+    try {
+      final statuses =
+          await MastodonRepository.instance.fetchAccountStatuses(accountId);
+      state = state.copyWith(statuses: statuses);
+    } catch (e) {
+      throw Exception('Failed to fetch statuses: $e');
+    } finally {
+      _setLoading(false);
+    }
+  }
+}
